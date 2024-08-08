fe0aaa651a5b8db779f61cd14134047d94275c09
Ashharja <rockashhar10@gmail.com>
2024-08-08 17:21:25 +0530

    Learnt Jevascript Conitionals

---
+++ Javascript/Javascript Conditionals/index.js
@@ -0,0 +1,26 @@
+console.log("This is a conditional Lesson")
+
+let age = 0;
+// let grace = 2;
+
+// age += grace;
+// console.log(age)
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+// console.log(age % grace)
+
+if(age!=18) {
+    console.log("You can Drive");
+}
+
+else if(age==0) {
+    console.log("Are you Kidding?")
+}
+
+else {
+    console.log("You cannot Drive");
+}
