b6a3e102fd247c17cf7e2152a80facbc67ca7678
Ashharja <rockashhar10@gmail.com>
2024-08-08 15:33:35 +0530

    Started Javascript

---
+++ Javascript/Introduction/script.js
@@ -0,0 +1,9 @@
+alert("Hello World");
+
+console.log("Code is Looking Like a Wow...")
+
+var a = prompt("Enter Your Number")
+
+var isTrue = confirm("Are you sure you want to leave this page?")
+
+console.log("Your number is " + a)
