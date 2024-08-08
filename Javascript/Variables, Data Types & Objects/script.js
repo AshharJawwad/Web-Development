5c01b58459bf634e2fd4d5feabe9bd6a9c64caaf
Ashharja <rockashhar10@gmail.com>
2024-08-08 16:19:09 +0530

    Learned JS Variables, Data Types and Objects

---
+++ Javascript/Variables, Data Types & Objects/script.js
@@ -0,0 +1,33 @@
+let a = 13;
+let b = 24;
+let c = "Ashhar";
+
+// console.log(a + b)
+// console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 5;
+    console.log(a)
+}
+console.log(a)
+
+let x = "Alok Bhai";
+let y = 22;
+let z = 33.9;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name: "Ashhar",
+    'job code': 5335
+}
+
+console.log(o);
+o.salary = "10cr";
+console.log(o);
+o.salary = "50cr";
+console.log(o)
