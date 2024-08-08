583d80001c8ede409bf0b281a361a3f15dc52fc1
Ashharja <rockashhar10@gmail.com>
2024-08-08 17:53:24 +0530

    Learnt Javascript Loops

---
+++ Javascript/Javascript Loops/index.js
@@ -0,0 +1,34 @@
+console.log("Learning Loops in Javascript");
+
+let a = 1;
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(a + i);
+// }
+
+// let obj = {
+//   name: "Ashhar Jawwad",
+//   role: "Programmer",
+//   company: "Storm Corporation",
+// };
+
+// for (const key in obj) {
+//   const element = obj[key];
+//   console.log(key, element);
+// }
+
+// for (const c of "Ashhar") {
+//     console.log(c)
+// }
+
+let i = 0;
+
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+do {
+    console.log(i)
+    i++;    
+} while (i<6);
