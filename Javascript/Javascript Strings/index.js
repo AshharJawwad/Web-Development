79af571e479675a2d6f29da16e3ffee85e875f49
Ashharja <rockashhar10@gmail.com>
2024-08-09 16:04:03 +0530

    Javascript Strings

---
+++ Javascript/Javascript Strings/index.js
@@ -0,0 +1,25 @@
+console.log("Learning Strings in Javascript")
+
+let a = "Ashhar";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+
+console.log(a.length)
+
+let real_name = "Ashhar"
+let friend = "Alok"
+console.log("His name is " + real_name + " and his friend's name is " + friend)
+console.log(`His name is ${real_name} and his friend's name is ${friend}`)
+
+let b = "Ahmad"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1, 4))
+
+console.log(b.replace("Ahm", "23"))
+console.log(b.concat(a))
